bf3b964124eb9cd1aeb74858e65e2debdf803eb3
Jide-codes <hezekiaholajide3@gmail.com>
2024-02-09 03:35:51 -1100

    working on the button functionality

--- index.js
+++ index.js
@@ -1,18 +1,91 @@
-function mult(a, b){
+const keys = document.querySelectorAll('.key')
+
+const display = document.querySelector('.display')
+
+let firstValue = " "
+let isFirstValue = false
+let secondValue = ""
+let isSecondValue = false
+let operator = ""
+let displayValue = 0
+
+
+// for (let key of keys) {
+//     keys[key].addEventListener('click', function (event) {
+//         let value = event.target.getAttribute('data-details')
+//         if(isFirstValue === 'false') {
+//             getFirstValue(value)
+//         }
+//         if (isSecondValue === false) {
+//             getSecondValue(value)
+//         }
+//     } )
+// }
+
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function (event) {
+        let value = event.target.getAttribute('data-details')
+        if (isFirstValue === false) {
+            getFirstValue(value)
+        }
+        if (secondValue === false) {
+            getSecondValue(value)
+        }
+   
+ })
+        
+}
+
+
+function getFirstValue(valueEl) {
+    display.textContent = " "
+    firstValue += valueEl
+    display.textContent = firstValue
+    firstValue = +firstValue
+   
+}
+
+function getSecondValue(valueEl) {
+    if (firstValue != " " && operator != " ") {
+        secondValue += valueEl
+        display.textContent = secondValue
+        secondValue = +secondValue
+     
+    }
+}
+
+function getSign() {
+    for (let key = 0; key < operator.length; key++) {
+        operator[key].addEventListener('click', function (event) {
+            operator = event.target.getAttribute('data-details')
+            isFirstValue = true
+        })
+    }
+}
+getSign()
+
+
+// multiply.addEventListener('click', function () {
+//     let ans = mult(0.0005, twoEl.textContent)
+//     answer.textContent = ans
+//     console.log(ans)
+// })
+
+function mult(a, b) {
     const answer = a * b;
-    const roundedAnswer = answer.toFixed(9)
+    const roundedAnswer = answer
     return roundedAnswer;
 }
 
 function subt(a, b) {
     const answer = a - b;
-    const roundedAnswer = answer.toFixed(9)
+    const roundedAnswer = Number(answer).toFixed(9)
     return roundedAnswer;
 }
 
 function addit(a, b) {
     const answer = a + b;
-    const roundedAnswer = answer.toFixed(9)
+    const roundedAnswer = Number(answer).toFixed(9)
     return roundedAnswer; 
 }
 
@@ -57,4 +130,4 @@ function perc(a) {
 }
 
 currentResult = perc(5);
-console.log(currentResult)
+// console.log(currentResult)
