a856592be1720c2278a2badf71fbaecaeb8ccd70
Onuh Andrew Somtochukwu <onuhandrew3@gmail.com>
2024-02-09 14:33:42 +0100

    Modified functions

--- index.js
+++ index.js
@@ -1,60 +1,75 @@
 function mult(a, b){
     const answer = a * b;
-    const roundedAnswer = answer.toFixed(9)
-    return roundedAnswer;
+    return answer;
 }
 
 function subt(a, b) {
     const answer = a - b;
-    const roundedAnswer = answer.toFixed(9)
-    return roundedAnswer;
+    return answer;
 }
 
 function addit(a, b) {
     const answer = a + b;
-    const roundedAnswer = answer.toFixed(9)
-    return roundedAnswer; 
+    return answer; 
 }
 
 function divi(a, b) {
     const answer = a / b;
-    const decimalIndex = answer.toString();
-    const indexOfDecimal = decimalIndex.indexOf(".");
-    if (indexOfDecimal !== -1) {
-        const decimalPlaces = decimalIndex.length - 1 - indexOfDecimal;
-        if (decimalPlaces >= 9) {
-            const backToNum = parseFloat(decimalIndex);
-            const roundedAnswer = backToNum.toFixed(9)
-            return roundedAnswer;
-        } else {
-            const backToNum = parseFloat(decimalIndex);
-        return backToNum;
-        }
-    } else {
-        const backToNum = parseInt(decimalIndex);
-        return backToNum;
-    }
+    // const decimalIndex = answer.toString();
+    // const indexOfDecimal = decimalIndex.indexOf(".");
+    // if (indexOfDecimal !== -1) {
+    //     const decimalPlaces = decimalIndex.length - 1 - indexOfDecimal;
+    //     if (decimalPlaces >= 9) {
+    //         const backToNum = parseFloat(decimalIndex);
+    //         const roundedAnswer = backToNum.toFixed(9)
+    //         return roundedAnswer;
+    //     } else {
+    //         const backToNum = parseFloat(decimalIndex);
+    //     return backToNum;
+    //     }
+    // } else {
+    //     const backToNum = parseInt(decimalIndex);
+    //     return backToNum;
+    // }
+    return answer;
 }
 
-function perc(a) {
+function perc(a, b) {
     const answer = a / 100;
-    const decimalIndex = answer.toString();
-    const indexOfDecimal = decimalIndex.indexOf(".");
-    if (indexOfDecimal !== -1) {
-        const decimalPlaces = decimalIndex.length - 1 - indexOfDecimal;
-        if (decimalPlaces >= 9) {
-            const backToNum = parseFloat(decimalIndex);
-            const roundedAnswer = backToNum.toFixed(9)
-            return roundedAnswer + "%";
-        } else {
-            const backToNum = parseFloat(decimalIndex);
-        return backToNum + "%";
-        }
-    } else {
-        const backToNum = parseInt(decimalIndex);
-        return backToNum + "%";
-    }
+    // const decimalIndex = answer.toString();
+    // const indexOfDecimal = decimalIndex.indexOf(".");
+    // if (indexOfDecimal !== -1) {
+    //     const decimalPlaces = decimalIndex.length - 1 - indexOfDecimal;
+    //     if (decimalPlaces >= 9) {
+    //         const backToNum = parseFloat(decimalIndex);
+    //         const roundedAnswer = backToNum.toFixed(9)
+    //         return roundedAnswer + "%";
+    //     } else {
+    //         const backToNum = parseFloat(decimalIndex);
+    //     return backToNum + "%";
+    //     }
+    // } else {
+    //     const backToNum = parseInt(decimalIndex);
+    //     return backToNum + "%";
+    // }
+    return answer;
 }
 
-currentResult = perc(5);
+function calc(a, b, params) {
+    if (params === mult) {
+        return mult(a, b);
+    } else if(params === subt){
+        return subt(a, b);
+    } else if(params === addit){
+        return addit(a, b);
+    } else if(params === divi){
+        return divi(a, b);
+    } else if(params === perc){
+        return perc(a);
+    }else {
+       return "Non Valid Input" 
+    } 
+}
+
+currentResult = calc(10, 2, perc);
 console.log(currentResult)
